de1802fba18aa0cb5bb76e87b9bfb2284b3ebc3f
Jan Abe de Waal <janabedewaal@protonmail.com>
2024-02-26 13:41:16 +0100

    Add dropdown list

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
-import {NgOptimizedImage} from "@angular/common";
+import {NgClass, NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    NgClass
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  show = false;
+
+  toggleList(): void {
+    this.show = !this.show;
+  }
 
   switchLanguage(language: string): void {
     const url = window.location.href.replace(/(\/nl|\/en-US)/, `/${language}`);
