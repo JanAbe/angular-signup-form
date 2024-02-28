f49045bee3109a20a21cd38eac147a4f62188ed9
Jan Abe de Waal <janabedewaal@protonmail.com>
2024-02-28 11:16:11 +0100

    Add cloudformation, improve language switching

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,24 +1,33 @@
 import { Component } from '@angular/core';
-import {NgClass, NgOptimizedImage} from "@angular/common";
+import {NgClass, NgIf, NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
     NgOptimizedImage,
-    NgClass
+    NgClass,
+    NgIf
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
   show = false;
+  selectedLanguage = this.getLanguageFromURL();
+
+  getLanguageFromURL(): string {
+    const url = window.location.pathname;
+    const paths = url.split('/');
+    return paths.filter(content => content !== "")[0];
+  }
 
   toggleList(): void {
     this.show = !this.show;
   }
 
   switchLanguage(language: string): void {
+    this.selectedLanguage = language;
     const url = window.location.href.replace(/(\/nl|\/en-US)/, `/${language}`);
     window.location.href = url;
   }
